87573a39ca11e245184a77a631e34e2f451c903b
khadai <33032060+khadai@users.noreply.github.com>
2018-02-19 18:02:48 +0200

    adding code of firstlab

---
+++ Java labs/lab1/WaterTap.java
@@ -0,0 +1,102 @@
+
+public class WaterTap {
+	    
+		private String name = "Its Name";
+		private String brand = "Its Brand";
+		private String bodyMaterial = "Its Material";
+		private int price;
+		private int quantityOfTaps;
+		
+		public static int totalQuantityOfTaps = 0;
+		
+		public WaterTap(){
+		}
+		
+		public WaterTap(String brand, String name, String bodyMaterial, int price) {
+			this.brand = brand;
+			this.name = name;
+			this.bodyMaterial = bodyMaterial;
+			this.price = price;
+		}
+		
+		public WaterTap(String brand, String name, String bodyMaterial, int price, int quantity) {
+			this.brand = brand;
+			this.name = name;
+			this.bodyMaterial = bodyMaterial;
+			this.price = price;
+			setQuantityOfTaps(quantity); 
+		}
+		
+		@Override
+		public String toString() {
+			String info = "This water tap of brand " + brand ;
+			info += " is called " + name;
+			info += ", its body material is " + bodyMaterial;
+			info += ", its price is $" + price;
+			info += ", quantity of this tap is " + quantityOfTaps + " pieces";
+			return info;
+		}
+		
+		public void printSum() {
+			String info = "There is " + this.quantityOfTaps;
+			info += " of taps, which called " + name;
+			info += ", there is "+ totalQuantityOfTaps + " taps total";
+			 System.out.println(info);
+		}
+		
+		public void resetValues(String brand, String name, 
+				String bodyMaterial, int price, int quantity){
+			setBrand(brand);
+			setName(name);
+			setBodyMaterial(bodyMaterial);
+			setPrice(price);
+			setQuantityOfTaps(quantity); 
+		}
+		
+		static void printStaticSum() {
+			 System.out.println("There is "+ totalQuantityOfTaps + " taps total");
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setBrand(String brand){
+			this.brand = brand;
+		}
+		
+		public String getBrand() {
+			return brand;
+		}
+		
+		public void setBodyMaterial(String bodyMaterial) {
+			this.bodyMaterial = bodyMaterial;
+		}
+		
+		public String getBodyMaterial() {
+			return bodyMaterial;
+		}
+		
+		public int getPrice() {
+			return price;
+		}
+		
+		public void setPrice(int price) {
+			this.price = price;
+		}
+		
+		public int getQuantityOfTaps() {
+			return quantityOfTaps;
+		}
+		
+		public void setQuantityOfTaps(int quantityOfTaps) {
+			totalQuantityOfTaps -= this.quantityOfTaps; 
+			this.quantityOfTaps = quantityOfTaps;
+			totalQuantityOfTaps += this.quantityOfTaps; 
+		}
+
+}
