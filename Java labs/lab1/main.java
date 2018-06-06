335a76e5d9dd1450df3e6e9efd2c73e2e1a82ebc
khadai <mykhailo.khadai@gmail.com>
2018-06-06 16:53:55 +0300

    added doc for "setQuantityOfTaps"

--- Java labs/lab1/main.java
+++ Java labs/lab1/main.java
@@ -1,33 +1,33 @@
 
 public class main {
 
-	public static void main(String[] args) {
-		
-		WaterTap sanluxVilla = new WaterTap();
-		System.out.println(sanluxVilla.toString());
-		sanluxVilla.resetValues("Sanlux Villa", "614-H05", "silumin", 13, 500);
-		System.out.println(sanluxVilla.toString());
+    public static void main(String[] args) {
 
-		WaterTap hansgrone = new WaterTap("HANSGROHE", "Metric Steel Optic", "brass", 803);
-		System.out.println(hansgrone.toString());
-		hansgrone.printSum();
-		System.out.println("");
+        WaterTap sanluxVilla = new WaterTap();
+        System.out.println(sanluxVilla.toString());
+        sanluxVilla.resetValues("Sanlux Villa", "614-H05", "silumin", 13, 500);
+        System.out.println(sanluxVilla.toString());
+
+        WaterTap hansgrone = new WaterTap("HANSGROHE", "Metric Steel Optic", "brass", 803);
+        System.out.println(hansgrone.toString());
+        hansgrone.printSum();
+        System.out.println("");
 
 
-		WaterTap tangoRio = new WaterTap("Tango Rio", "04", "brass", 35, 600);
-		System.out.println(tangoRio.toString());
-		System.out.println("");
+        WaterTap tangoRio = new WaterTap("Tango Rio", "04", "brass", 35, 600);
+        System.out.println(tangoRio.toString());
+        System.out.println("");
 
-		
-		sanluxVilla.printSum();
-		tangoRio.printSum();
-		hansgrone.printSum();
-		
-		hansgrone.setQuantityOfTaps(200);
-		hansgrone.printSum();
-		
-		WaterTap.printStaticSum();
-		
-	}
+
+        sanluxVilla.printSum();
+        tangoRio.printSum();
+        hansgrone.printSum();
+
+        hansgrone.setQuantityOfTaps(200);
+        hansgrone.printSum();
+
+        WaterTap.printStaticSum();
+
+    }
 
 }
